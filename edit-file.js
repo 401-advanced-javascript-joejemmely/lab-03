f64f97be9981f5365b13edc94ec669720ea95170
Joé Jemmely <joe.jemmely@gmail.com>
2019-05-15 15:02:20 -0700

    Part 1

---
+++ edit-file.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const { promisify } = require('util');
+const fs = require('fs');
+
+// Promisify the functions
+const readFileAsync = promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
+
+const files = process.argv.slice(2);
+
+const randomNumber = Math.random();
+
+async function readUpdateAndReread(file) {
+  try {
+    // Read
+    const contentBefore = await readFileAsync(file, { encoding: 'utf8' });
+    console.log('Content before:', contentBefore);
+
+    // Update
+    await writeFileAsync(file, randomNumber);
+
+    const contentAfter = await readFileAsync(file, { encoding: 'utf8' });
+    console.log('Content after:', contentAfter);
+  } catch (err) {
+    console.log('Error:', err);
+  }
+}
+
+readUpdateAndReread(files[0]);
